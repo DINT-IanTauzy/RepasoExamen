1852b4e73bddad41fb7dd3d273128fec58986a03
IAN ESTEBAN TAUZY LAMAGNA <iantaulam@alu.edu.gva.es>
2023-02-04 19:43:20 +0100

    Terminado

---
+++ RepasoExamen/Convertidores/ConvertidorPrecio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RepasoExamen.Convertidores
+{
+    class ConvertidorPrecio : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //int num = (int)value;
+            
+            if(value is null)
+            {
+                return "";
+            }
+            else
+            {
+                return value + "€";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
